213ca09734e22cbe5e97c1d8fb533122146a1e9a
Hoang-Nguyen-Huy <hoangmegamind1302@gmail.com>
2024-12-09 15:55:15 +0700

    feat: 🎸 config cors

--- server/index.js
+++ server/index.js
@@ -1,9 +1,19 @@
-import { PrismaClient } from "@prisma/client";
 import express from "express";
 import funcProductRoute from "./routes/product.route.js";
+import cors from "cors";
 
 const app = express();
 const port = 3000;
+
+app.use(cors({ origin: "http://localhost:5173 " }));
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 
 app.use(express.json());
 
