fe0bd6bcb8055d5473969321985132053f91fbd0
Hoang-Nguyen-Huy <hoangmegamind1302@gmail.com>
2024-12-09 21:02:56 +0700

    feat: 🎸 style footer and call api get by id

--- app/apis/productManagementApi.ts
+++ app/apis/productManagementApi.ts
@@ -12,3 +12,16 @@ export const getPaginationProduct = async () => {
     throw error;
   }
 };
+
+export const getProductById = async (id: Number) => {
+  try {
+    const response = await fetch(`${baseUrl}/${id}`);
+    if (!response.ok) {
+      throw new Error("Network response was not ok");
+    }
+    return await response.json();
+  } catch (error) {
+    console.error("Error fetching orchids:", error);
+    throw error;
+  }
+};
--- app/components/ui/footer.tsx
+++ app/components/ui/footer.tsx
@@ -1,4 +1,5 @@
 import { Link } from "react-router-dom";
+import "~/styles/footer.css";
 
 export default function Footer() {
   const linkStyle =
@@ -21,15 +22,14 @@ export default function Footer() {
                   strokeLinecap='round'
                   strokeLinejoin='round'
                   strokeWidth={2}
-                  d='M12 6.253v13m0-13C10.832 5.477 9.246 5 7.5 5S4.168 5.477 3 6.253v13C4.168 18.477 5.754 18 7.5 18s3.332.477 4.5 1.253m0-13C13.168 5.477 14.754 5 16.5 5c1.747 0 3.332.477 4.5 1.253v13C19.832 18.477 18.247 18 16.5 18c-1.746 0-3.332.477-4.5 1.253'
+                  d='M12 6.253v13m0-13C10.832 5.477 9.246 5 7.5 5S4.168 5.477 3 6.253v13C4.168 18.477 5.754 18 7.5 18s3.332.477 4.5 1.253m0-13C13.168 5.477 14.754 5 16.5 5c1.747 0 3.332.477 4.5 1.253'
                 />
               </svg>
-              <h5 className='text-xl font-bold text-white'>
-                Product Management
-              </h5>
+              <h5 className='text-xl font-bold text-white'>Orchid Haven</h5>
             </div>
             <p className='text-sm text-gray-400'>
-              Great products start with understanding people.
+              Discover the beauty and diversity of orchids. Our passion is to
+              share knowledge and inspire orchid enthusiasts worldwide.
             </p>
           </div>
           <div>
@@ -37,7 +37,7 @@ export default function Footer() {
               Quick Links
             </h6>
             <div className='flex flex-col'>
-              <Link to='#' className={linkStyle}>
+              <Link to='/fer-lab1/' className={linkStyle}>
                 <svg
                   xmlns='http://www.w3.org/2000/svg'
                   className='h-5 w-5 inline mr-2'
@@ -54,7 +54,7 @@ export default function Footer() {
                 </svg>
                 Home
               </Link>
-              <Link to='#' className={linkStyle}>
+              <Link to='/fer-lab1/natural' className={linkStyle}>
                 <svg
                   xmlns='http://www.w3.org/2000/svg'
                   className='h-5 w-5 inline mr-2'
@@ -69,9 +69,9 @@ export default function Footer() {
                     d='M12 6.253v13m0-13C10.832 5.477 9.246 5 7.5 5S4.168 5.477 3 6.253v13C4.168 18.477 5.754 18 7.5 18s3.332.477 4.5 1.253m0-13C13.168 5.477 14.754 5 16.5 5c1.747 0 3.332.477 4.5 1.253'
                   />
                 </svg>
-                Best Sellers
+                Special Orchid
               </Link>
-              <Link to='#' className={linkStyle}>
+              <Link to='/fer-lab1/news' className={linkStyle}>
                 <svg
                   xmlns='http://www.w3.org/2000/svg'
                   className='h-5 w-5 inline mr-2'
@@ -86,9 +86,9 @@ export default function Footer() {
                     d='M19 20H5a2 2 0 01-2-2V6a2 2 0 012-2h10a2 2 0 012 2v1m2 13a2 2 0 01-2-2V7m2 13a2 2 0 002-2V9a2 2 0 00-2-2h-2m-4-3H9M7 16h6M7 8h6v4H7V8z'
                   />
                 </svg>
-                Product News
+                Orchid News
               </Link>
-              <Link to='#' className={linkStyle}>
+              <Link to='/fer-lab1/contact' className={linkStyle}>
                 <svg
                   xmlns='http://www.w3.org/2000/svg'
                   className='h-5 w-5 inline mr-2'
@@ -112,7 +112,7 @@ export default function Footer() {
               Stay Connected
             </h6>
             <p className='text-sm text-gray-400 mb-4'>
-              Follow us on social media for daily products inspiration and tips.
+              Follow us on social media for daily orchid inspiration and tips.
             </p>
             <div className='flex space-x-4'>
               <a
@@ -161,7 +161,7 @@ export default function Footer() {
                 >
                   <path
                     fillRule='evenodd'
-                    d='M12.315 2c2.43 0 2.784.013 3.808.06 1.064.049 1.791.218 2.427.465a4.902 4.902 0 011.772 1.153 4.902 4.902 0 011.153 1.772c.247.636.416 1.363.465 2.427.048 1.067.06 1.407.06 4.123v.08c0 2.643-.012 2.987-.06 4.043-.049 1.064-.218 1.791-.465 2.427a4.902 4.902 0 01-1.153 1.772 4.902 4.902 0 01-1.772 1.153c-.636.247-1.363.416-2.427.465-1.067.048-1.407.06-4.123.06h-.08c-2.643 0-2.987-.012-4.043-.06-1.064-.049-1.791-.218-2.427-.465a4.902 4.902 0 01-1.772-1.153 4.902 4.902 0 01-1.772-1.153c-.247-.636-.416-1.363-.465-2.427-.047-1.024-.06-1.379-.06-3.808v-.63c0-2.43.013-2.784.06-3.808.049-1.064.218-1.791.465-2.427a4.902 4.902 0 011.153-1.772A4.902 4.902 0 015.45 2.525c.636-.247 1.363-.416 2.427-.465C8.901 2.013 9.256 2 11.685 2h.63zm-.081 1.802h-.468c-2.456 0-2.784.011-3.807.058-.975.045-1.504.207-1.857.344-.467.182-.8.398-1.15.748-.35.35-.566.683-.748 1.15-.137.353-.3.882-.344 1.857-.047 1.023-.058 1.351-.058 3.807v.468c0 2.456.011 2.784.058 3.807.045.975.207 1.504.344 1.857.182.466.399.8.748 1.15.35.35.683.566 1.15.748.353.137.882.3 1.857.344 1.054.048 1.37.058 4.041.058h.08c2.597 0 2.917-.01 3.96-.058.976-.045 1.505-.207 1.858-.344.466-.182.8-.398 1.15-.748.35-.35.566-.683.748-1.15.137-.353.3-.882.344-1.857.048-1.055.058-1.37.058-4.041v-.08c0-2.597-.01-2.917-.058-3.96-.045-.976-.207-1.505-.344-1.858a3.097 3.097 0 00-.748-1.15 3.098 3.098 0 00-1.15-.748c-.353-.137-.882-.3-1.857-.344-1.023-.047-1.351-.058-3.807-.058zM12 6.865a5.135 5.135 0 110 10.27 5.135 5.135 0 010-10.27zm0 1.802a3.333 3.333 0 100 6.666 3.333 3.333 0 000-6.666zm5.338-3.205a1.2 1.2 0 110 2.4 1.2 1.2 0 010-2.4z'
+                    d='M12.315 2c2.43 0 2.784.013 3.808.06 1.064.049 1.791.218 2.427.465a4.902 4.902 0 011.772 1.153 4.902 4.902 0 011.153 1.772c.247.636.416 1.363.465 2.427.048 1.067.06 1.407.06 4.123v.08c0 2.643-.012 2.987-.06 4.043-.049 1.064-.218 1.791-.465 2.427a4.902 4.902 0 01-1.153 1.772 4.902 4.902 0 01-1.772 1.153c-.636.247-1.363.416-2.427.465-1.067.048-1.407.06-4.123.06h-.08c-2.643 0-2.987-.012-4.043-.06-1.064-.049-1.791-.218-2.427-.465a4.902 4.902 0 01-1.153 1.772 4.902 4.902 0 01-1.772 1.153c-.247-.636-.416-1.363-.465-2.427-.047-1.024-.06-1.379-.06-3.808v-.63c0-2.43.013-2.784.06-3.808.049-1.064.218-1.791.465-2.427a4.902 4.902 0 011.153-1.772A4.902 4.902 0 015.45 2.525c.636-.247 1.363-.416 2.427-.465C8.901 2.013 9.256 2 11.685 2h.63zm-.081 1.802h-.468c-2.456 0-2.784.011-3.807.058-.975.045-1.504.207-1.857.344-.467.182-.8.398-1.15.748-.35.35-.566.683-.748 1.15-.137.353-.3.882-.344 1.857-.047 1.023-.058 1.351-.058 3.807v.468c0 2.456.011 2.784.058 3.807.045.975.207 1.504.344 1.857.182.466.399.8.748 1.15.35.35.683.566 1.15.748.353.137.882.3 1.857.344 1.054.048 1.37.058 4.041.058h.08c2.597 0 2.917-.01 3.96-.058.976-.045 1.505-.207 1.858-.344.466-.182.8-.398 1.15-.748.35-.35.566-.683.748-1.15.137-.353.3-.882.344-1.857.048-1.055.058-1.37.058-4.041v-.08c0-2.597-.01-2.917-.058-3.96-.045-.976-.207-1.505-.344-1.858a3.097 3.097 0 00-.748-1.15 3.098 3.098 0 00-1.15-.748c-.353-.137-.882-.3-1.857-.344-1.023-.047-1.351-.058-3.807-.058zM12 6.865a5.135 5.135 0 110 10.27 5.135 5.135 0 010-10.27zm0 1.802a3.333 3.333 0 100 6.666 3.333 3.333 0 000-6.666zm5.338-3.205a1.2 1.2 0 110 2.4 1.2 1.2 0 010-2.4z'
                     clipRule='evenodd'
                   />
                 </svg>
@@ -189,8 +189,7 @@ export default function Footer() {
         <div className='my-8 border-t border-gray-700'></div>
         <div className='flex flex-col sm:flex-row justify-between items-center'>
           <p className='text-sm text-gray-400 mb-4 sm:mb-0'>
-            © {new Date().getFullYear()} Product Management. All rights
-            reserved.
+            © {new Date().getFullYear()} Orchid Haven. All rights reserved.
           </p>
           <div>
             <Link to='#' className={`${linkStyle} mr-4 inline-block`}>
